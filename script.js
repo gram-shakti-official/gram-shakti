cf2691bf27adab53cdb739e0c44c79e016141e6c
gram-shakti-official <pinky123metro01@gmail.com>
2026-03-06 06:48:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,82 @@
-function showMessage(msg){
-alert(msg);
-}
+// REGISTER USER
 
 function registerUser(){
-let name = document.getElementById("name").value;
-let mobile = document.getElementById("mobile").value;
-let role = document.getElementById("role").value;
+
+var name = document.getElementById("name").value;
+var mobile = document.getElementById("mobile").value;
+var type = document.getElementById("type").value;
+var work = document.getElementById("work").value;
+var city = document.getElementById("city").value;
 
 if(name=="" || mobile==""){
 alert("Please fill all fields");
 return;
 }
 
-localStorage.setItem("user_name",name);
-localStorage.setItem("user_mobile",mobile);
-localStorage.setItem("user_role",role);
+var id = new Date().getTime();
+
+firebase.database().ref("users/"+id).set({
+
+name:name,
+mobile:mobile,
+type:type,
+work:work,
+city:city
+
+});
 
 alert("Registration Successful");
 
-window.location.href="index.html";
+window.location="index.html";
+
 }
 
-function logout(){
-localStorage.clear();
-window.location.href="login.html";
+
+// LOGIN USER
+
+function loginUser(){
+
+var mobile = document.getElementById("mobile").value;
+
+if(mobile==""){
+alert("Enter mobile number");
+return;
 }
 
-function checkLogin(){
-let name = localStorage.getItem("user_name");
+var ref = firebase.database().ref("users");
 
-if(!name){
-window.location.href="login.html";
+ref.once("value",function(snapshot){
+
+var data = snapshot.val();
+
+var found = false;
+
+for(var key in data){
+
+if(data[key].mobile == mobile){
+
+found = true;
+
+localStorage.setItem("user", JSON.stringify(data[key]));
+
+break;
+
 }
+
+}
+
+if(found){
+
+alert("Login Successful");
+
+window.location="index.html";
+
+}else{
+
+alert("User not found");
+
+}
+
+});
+
 }
