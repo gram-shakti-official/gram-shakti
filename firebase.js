232a6435982fb1c41fe4d2776e70600cd8da4977
gram-shakti-official <pinky123metro01@gmail.com>
2026-03-06 06:15:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
 // Firebase Config
-
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_PROJECT_ID.appspot.com",
-messagingSenderId: "123456789",
-appId: "APP_ID"
+  apiKey: "AIzaSyA0J8TCjU2wxDNmT2O4VMfchY06arTO-O0",
+  authDomain: "gram-shakti-app-2367b.firebaseapp.com",
+  databaseURL: "https://gram-shakti-app-2367b-default-rtdb.firebaseio.com",
+  projectId: "gram-shakti-app-2367b",
+  storageBucket: "gram-shakti-app-2367b.firebasestorage.app",
+  messagingSenderId: "606732424351",
+  appId: "1:606732424351:web:5802ac51402c6921233eaa",
+  measurementId: "G-NCEP6BBTWQ"
 };
 
-// Initialize Firebase
+// Firebase Start
 firebase.initializeApp(firebaseConfig);
 
+// Database
 const db = firebase.database();
