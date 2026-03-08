5fdf490ba025a4ab56ccde03553f5a450708fed7
gram-shakti-official <pinky123metro01@gmail.com>
2026-03-08 10:44:31 +0530

    Add location data for multiple Indian states
    Added location data for Rajasthan, Uttar Pradesh, and Madhya Pradesh with cities and towns.

---
+++ location.js
@@ -0,0 +1,60 @@
+var locationData = {
+
+"Rajasthan":{
+
+"Jaipur":[
+"Jaipur City",
+"Chomu",
+"Shahpura",
+"Amer",
+"Phulera"
+],
+
+"Alwar":[
+"Alwar City",
+"Behror",
+"Bansur",
+"Kishangarh Bas"
+],
+
+"Sikar":[
+"Sikar City",
+"Fatehpur",
+"Lachhmangarh",
+"Neem Ka Thana"
+]
+
+},
+
+"Uttar Pradesh":{
+
+"Lucknow":[
+"Lucknow City",
+"Malihabad",
+"Mohanlalganj"
+],
+
+"Kanpur":[
+"Kanpur City",
+"Bilhaur",
+"Ghatampur"
+]
+
+},
+
+"Madhya Pradesh":{
+
+"Bhopal":[
+"Bhopal City",
+"Berasia"
+],
+
+"Indore":[
+"Indore City",
+"Sanwer",
+"Mhow"
+]
+
+}
+
+};
